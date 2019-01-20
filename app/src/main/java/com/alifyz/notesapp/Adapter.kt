36a6b69224b0871c8b4e1d2e3b56110fca0d8ede
alifyz <double3neck>
2019-01-20 18:30:49 -0200

    implementação final

--- app/src/main/java/com/alifyz/notesapp/Adapter.kt
+++ app/src/main/java/com/alifyz/notesapp/Adapter.kt
@@ -8,21 +8,32 @@ import android.widget.TextView
 
 class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>() {
 
+    var dataSet : List<NotesEntity> = listOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.notes, parent, false))
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return dataSet.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.note.text = dataSet.get(position).texto
+        holder.priority.text = dataSet.get(position).prioridade
     }
 
     inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         val note = itemView.findViewById<TextView>(R.id.notes)
         val priority = itemView.findViewById<TextView>(R.id.priority)
+    }
+
+    fun setData(newData : List<NotesEntity>) {
+        dataSet = newData
+        notifyDataSetChanged()
+    }
+
+    fun getData(position: Int) : NotesEntity {
+        return dataSet.get(position)
     }
 }
